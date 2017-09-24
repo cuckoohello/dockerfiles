74c1b3a961d5579f9bb10897f03d2eb2c1ca3073
Yafeng Shan <cuckoo@kokonur.me>
2017-09-24 23:52:11 +0800

    jupyter: add root password and openssh client

--- jupyter/entrypoint.sh
+++ jupyter/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+echo "root:$ROOT_PASSWORD" | chpasswd
 useradd -m $USER -u $UID -s /bin/bash
 mkdir -p /workspace/.jupyter/ && ln -fs /workspace/.jupyter /home/$USER/.jupyter && chown -R $USER:$USER /workspace /home/$USER /opt/conda
 export SHELL=/bin/bash
