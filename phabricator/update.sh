6b88b6734bfd5feb3e411abf938b4e0a648ed69c
Yafeng Shan <cuckoo@kokonur.me>
2018-01-27 01:14:14 +0800

    [phabricator] add notification server

--- phabricator/update.sh
+++ phabricator/update.sh
@@ -18,10 +18,32 @@ fi
 if [ ! -e phabricator ]
 then
   git clone https://github.com/phacility/phabricator.git -b stable --depth=1
+  cd ~/phacility/phabricator/support/aphlict/server/ && npm install ws
 else
   (cd phabricator && git pull --rebase)
 fi
+
+# Parse $NOTIFICATION_CLIENT and $NOTIFICATION_ADMIN
+NOTIFICATION_CLIENT_PROTOCOL="$(echo $PHABRICATOR_URL | sed -e's@^\(.*\)://.*@\1@g')"
+NOTIFICATION_CLIENT=$(echo $PHABRICATOR_URL | sed -e"s@$NOTIFICATION_CLIENT_PROTOCOL://@@g")
+
+if echo $NOTIFICATION_CLIENT | grep -q ':' ; then
+  NOTIFICATION_CLIENT_HOST="$(echo $NOTIFICATION_CLIENT | cut -d: -f1)"
+  NOTIFICATION_CLIENT_PORT="$(echo $NOTIFICATION_CLIENT | cut -d: -f2)"
+else
+  NOTIFICATION_CLIENT_HOST=$NOTIFICATION_CLIENT
+  if [ "$NOTIFICATION_CLIENT_PROTOCOL" = "https" ]
+    NOTIFICATION_CLIENT_PORT=443
+  else
+    NOTIFICATION_CLIENT_PORT=80
+  fi
+fi
+
+export NOTIFICATION_CLIENT_PROTOCOL NOTIFICATION_CLIENT_HOST NOTIFICATION_CLIENT_PORT
+
+# Apply configuration template
 cat /local.init.json | envsubst > /home/git/phacility/phabricator/conf/local/local.json
+
 cp /preamble.php /home/git/phacility/phabricator/support/preamble.php
 
 cd ~/phacility/phabricator && ./bin/storage upgrade --force
